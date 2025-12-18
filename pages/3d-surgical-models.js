4d380ced01c6cab0033908da3fd19ef05fa425d6
ajrkhan1 <ajr.rgs@gmail.com>
2025-12-18 14:30:15 +0530

    kk
    ol

--- pages/3d-surgical-models.js
+++ pages/3d-surgical-models.js
@@ -58,8 +58,8 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>3D Surgical Models - Curewith3D</title>
-				<meta name="description" content="Your clarity guides you as you work through the various layers of the 3D human anatomy. With our 3D surgical models, the boundaries of radiology and reality" />
+				<title>Custom 3D Printed Anatomical Models | Curewith3D</title>
+				<meta name="description" content="CT/MRI based anatomical bone models, dental models, tumor models, to facilitate pre-surgery planning for complex orthopedic, facial trauma, OMF & cranial cases"/>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 				<link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" />
