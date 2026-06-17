c845d794581360ebc3598b483cfc868ddb4acc4c
ajrkhan1 <ajr.rgs@gmail.com>
2026-06-17 15:42:00 +0530

    hh
    v

--- pages/_document.js
+++ pages/_document.js
@@ -52,6 +52,19 @@ export default function Document() {
           `,
             }}
           />
+
+           <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x8e4uk80pr");
+   `,
+            }}
+          />
+
     </Head>
       <body>
         <Main />
