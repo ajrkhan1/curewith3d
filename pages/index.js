9f4856c8529950f3accee2946524bda68f862f5c
ajrkhan1 <ajr.rgs@gmail.com>
2026-06-27 12:18:04 +0530

    df
    sd

--- pages/index.js
+++ pages/index.js
@@ -228,7 +228,7 @@ export default function Home() {
 				<link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
                 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>
 
-				<script
+				<script type="application/ld+json"
           dangerouslySetInnerHTML={
             {__html:`
               {
